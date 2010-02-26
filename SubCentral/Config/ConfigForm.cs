58804c191a9377e5e19ffc5db0cad07210067931
conrad.john <conrad.john@d735d0e0-0b73-11df-8f06-1508ab885915>
2010-02-26 20:49:08 +0000

    Initial commit.

---
+++ SubCentral/Config/ConfigForm.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MediaPortal.GUI.Library;
+using NLog;
+
+namespace SubCentral.ConfigForm {
+    public partial class ConfigForm : Form, ISetupForm {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        private SubCentralCore core = SubCentralCore.Instance;
+
+        public ConfigForm() {
+            InitializeComponent();
+        }
+
+        #region ISetupForm Members
+
+        // Returns the name of the plugin which is shown in the plugin menu 
+        public string PluginName() {
+            return "SubCentral";
+        }
+
+        // Returns the description of the plugin is shown in the plugin menu     
+        public string Description() {
+            return "A plugin that will allow you to download subtitles for movies or TV shows.";
+        }
+
+        // Returns the author of the plugin which is shown in the plugin menu     
+        public string Author() {
+            return "";
+        }
+
+        // show the setup dialog    
+        public void ShowPlugin() {
+            core.Initialize();
+            logger.Info("Launching Configuration Screen");
+            this.ShowDialog();
+        }
+
+        // Indicates whether plugin can be enabled/disabled      
+        public bool CanEnable() {
+            return true;
+        }
+
+        // get ID of windowplugin belonging to this setup    
+        public int GetWindowId() {
+            return 84623;
+        }
+
+        // Indicates if plugin is enabled by default
+        public bool DefaultEnabled() {
+            return true;
+        }
+        
+        // indicates if a plugin has its own setup screen     
+        public bool HasSetup() {
+            return true;
+        }
+
+        public bool GetHome(out string strButtonText, out string strButtonImage, out string strButtonImageFocus, out string strPictureImage) {
+            strButtonText = "SubCentral";
+            strButtonImage = String.Empty;
+            strButtonImageFocus = String.Empty;
+            strPictureImage = "hover_subcentral.png";
+            return true;
+        }
+
+        #endregion
+    }
+}
---
+++ SubCentral/PluginHandlers/MovingPicturesHandler.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using MediaPortal.Plugins.MovingPictures;
+using MediaPortal.Plugins.MovingPictures.MainUI;
+using MediaPortal.Plugins.MovingPictures.Database;
+using NLog;
+using System.Reflection;
+
+namespace SubCentral.PluginHandlers {
+    internal class MovingPicturesHandler : PluginHandler {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        private MovieBrowser browser;
+
+        public override int ID {
+            get { return 96742; }
+        }
+
+        public override string PluginName {
+            get { return "Moving Pictures"; }
+        }
+
+        // The video file we want to grab subtitles for.
+        public override FileInfo File {
+            get { return _file; }
+        }
+        private FileInfo _file = null;
+
+        // The name of the movie we are pulling subtitles for.
+        public override string Description {
+            get { return _description; }
+        }
+        private string _description = "";
+
+        // retrieves info from Moving Pictures
+        protected override bool GrabFileDetails() {
+            try {
+                browser = MovingPicturesCore.Browser; 
+                DBMovieInfo selectedMovie = browser.SelectedMovie;
+                List<DBLocalMedia> localMedia = selectedMovie.LocalMedia;
+                _description = string.Format("{0} ({1})", selectedMovie.Title, selectedMovie.Year);
+                _file = new FileInfo(localMedia[0].FullPath);
+                return true;
+            }
+            catch (Exception) {
+                logger.Error("Unexpected error when pulling data from Moving Pictures.");
+                return false;
+            }
+
+        }
+
+        protected override bool IsAvailable() {
+            if (!IsAssemblyAvailable("MovingPictures", new Version(1,0,3)))
+                return false;
+
+            return true;
+        }
+    }
+}
