0c14ac14974d3b18df0ad4b35ce33a727930022c
mitja.skuver <mitja.skuver@d735d0e0-0b73-11df-8f06-1508ab885915>
2010-06-19 18:36:40 +0000

    AssemblyVersion is now updated on pre-build using TortoiseSVN utils

--- trunk/SubCentral/Properties/AssemblyInfo.cs
+++ trunk/SubCentral/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SubCentral")]
-[assembly: AssemblyCopyright("Copyright ©  2010")]
+[assembly: AssemblyCopyright("Copyright © 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.1.0")]
-[assembly: AssemblyFileVersion("0.9.1.0")]
+// AssemblyVersion is updated by Pre-Build step
+// Assume TortoiseSVN is in enviroment to run: subwcrev.exe
+//[assembly: AssemblyVersion("0.9.1.0")]
+//[assembly: AssemblyFileVersion("0.9.1.0")]
---
+++ trunk/SubCentral/Properties/VersionInfo.cs
@@ -0,0 +1,2 @@
+﻿[assembly: System.Reflection.AssemblyVersion("0.9.1.54")]
+[assembly: System.Reflection.AssemblyFileVersion("0.9.1.54")]
