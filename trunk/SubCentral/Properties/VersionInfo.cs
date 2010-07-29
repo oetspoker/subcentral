61cc084ad44dd9173e019534b27f0409d1f6e3b1
mitja.skuver <mitja.skuver@d735d0e0-0b73-11df-8f06-1508ab885915>
2010-07-29 10:05:20 +0000

    Updated External references (MediaPortal v1.1)

--- trunk/SubCentral/Properties/VersionInfo.cs
+++ trunk/SubCentral/Properties/VersionInfo.cs
@@ -1,2 +1,2 @@
-﻿[assembly: System.Reflection.AssemblyVersion("0.9.1.72")]
-[assembly: System.Reflection.AssemblyFileVersion("0.9.1.72")]
+﻿[assembly: System.Reflection.AssemblyVersion("0.9.1.73")]
+[assembly: System.Reflection.AssemblyFileVersion("0.9.1.73")]
