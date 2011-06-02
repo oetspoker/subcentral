3831159fc8b6a1518a83a19c41c0518cbaf4ca60
mitja.skuver <mitja.skuver@d735d0e0-0b73-11df-8f06-1508ab885915>
2011-06-02 21:40:18 +0000

    Issue 41: Support for MyVideos Support for MyFilms (thanks to Guzzi) Improved support for handlers not having database subtitle fields (MyVideos or MyFilms) Update assemblies in externals

--- SubCentral/PluginHandlers/PluginHandlerManager.cs
+++ SubCentral/PluginHandlers/PluginHandlerManager.cs
@@ -58,12 +58,12 @@ namespace SubCentral.PluginHandlers {
 
             // log all the active plugin handlers
             foreach (PluginHandler h in handlers.Values) {
-                if (h.Type == PluginHandlerType.BASIC && h.Available) logger.Info("Enabled Plugin: {0}", h.PluginName);
+                if ((h.Type == PluginHandlerType.BASIC || h.Type == PluginHandlerType.BASICWITHOUTDB) && h.Available) logger.Info("Enabled Plugin: {0}", h.PluginName);
             }
 
             // log all the inactive plugin handlers
             foreach (PluginHandler h in handlers.Values)
-                if (h.Type == PluginHandlerType.BASIC && !h.Available) logger.Info("Unavailable or Outdated Plugin: {0}", h.PluginName);
+                if ((h.Type == PluginHandlerType.BASIC || h.Type == PluginHandlerType.BASICWITHOUTDB) && !h.Available) logger.Info("Unavailable or Outdated Plugin: {0}", h.PluginName);
         }
     }
 }
--- SubCentral/PluginHandlers/PluginHandlerType.cs
+++ SubCentral/PluginHandlers/PluginHandlerType.cs
@@ -3,6 +3,7 @@ namespace SubCentral.PluginHandlers {
     public enum PluginHandlerType {
         UNDEFINED, // all
         BASIC,
+        BASICWITHOUTDB, // doesn't have subtitle fields in internal db
         CUSTOM,
         MANUAL
     }
