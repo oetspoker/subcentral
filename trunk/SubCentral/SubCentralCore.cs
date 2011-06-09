df764753a98476e6260044429fb9c2772f1b54b9
mitja.skuver <mitja.skuver@d735d0e0-0b73-11df-8f06-1508ab885915>
2011-06-09 22:54:44 +0000

    Minimum SubtitleDownloader version set to 2.9

--- trunk/SubCentral/SubCentralCore.cs
+++ trunk/SubCentral/SubCentralCore.cs
@@ -16,7 +16,7 @@ using SubCentral.Utils;
 namespace SubCentral {
     public class SubCentralCore {
 
-        public static readonly Version SubtitleDownloaderVersion = new Version(2,2);
+        public static readonly Version SubtitleDownloaderVersion = new Version(2, 9);
 
         private static Logger logger = LogManager.GetCurrentClassLogger();
 
