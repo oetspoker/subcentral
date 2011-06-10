a514e10a4b65eb0c1d2447f107335c015d5cb25d
mitja.skuver <mitja.skuver@d735d0e0-0b73-11df-8f06-1508ab885915>
2011-06-10 10:00:02 +0000

    Added sanity checks to subtitle returned from downloader

---
+++ SubCentral/GUI/Extensions/GeneralExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NLog;
+
+namespace SubCentral.GUI.Extensions {
+    public static class GeneralExtensions {
+          private static Logger logger = LogManager.GetCurrentClassLogger();
+
+      #region String extensions
+      public static bool IsNullOrWhiteSpace(this string value)
+      {
+          return (string.IsNullOrEmpty(value) || string.IsNullOrEmpty(value.Trim()));
+      }
+      #endregion
+    }
+}
