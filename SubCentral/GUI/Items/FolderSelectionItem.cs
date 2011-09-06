f9e9f8ff35884f4b54c76d260a052dd44a7e5813
mitja.skuver <mitja.skuver@d735d0e0-0b73-11df-8f06-1508ab885915>
2011-09-06 18:21:20 +0000

    IDMF branch: started work, does not compile - still many to do

--- SubCentral/GUI/Items/FolderSelectionItem.cs
+++ SubCentral/GUI/Items/FolderSelectionItem.cs
@@ -1,12 +1,23 @@
-﻿
+﻿using System.Collections.Generic;
+
 namespace SubCentral.GUI.Items {
     public struct FolderSelectionItem {
         public string ItemID { get; set; }
-        public string FolderName { get; set; }
-        public string FolderName2 { get; set; }
+        public string FolderName1 { get; set; }
+        // list of folder names count matching video files
+        public List<string> FolderNames { get; set; }
         public string OriginalFolderName { get; set; }
         public bool WasRelative { get; set; }
         public FolderErrorInfo FolderErrorInfo { get; set; }
+        private Dictionary<string, FolderErrorInfo> folderErrorInfos;
+        public Dictionary<string, FolderErrorInfo> FolderErrorInfos { 
+            get {
+                if (folderErrorInfos == null) {
+                    folderErrorInfos = new Dictionary<string, FolderErrorInfo>();
+                }
+                return folderErrorInfos;
+            }
+        }
         //public bool Existing { get; set; }
         //public bool Writable { get; set; }
         public object Tag { get; set; }
